b7f8bf43dc0b057778d2d511a11f15f2b057bc20
cleybersilva <cleyber.silva@live.com>
2025-05-03 23:24:11 -0300

    Correção no destroy_config.json

--- infra/envs/main.tf
+++ infra/envs/main.tf
@@ -1,8 +1,4 @@
-data "aws_s3_bucket" "existing_bucket" {
-  bucket = var.bucket_name
-}
-
 resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-  count  = length(data.aws_s3_bucket.existing_bucket.id) == 0 ? 1 : 0
+  bucket        = var.bucket_name
+  force_destroy = true
 }
